f5f95e7186202e1b17e02865b49572a3cf8474e0
Nawin chander <nawinnapzy289@gmail.com>
2026-03-17 15:01:53 +0530

    changes the file and code

--- Advance/Remove Nth Node.js
+++ Advance/Remove Nth Node.js
@@ -2,6 +2,35 @@
 
 /// Use two pointers with gap = n
 
+function removeNthFromEnd(head, n) {
+  let dummy = new Node(0);
+  dummy.next = head;
+
+  let first = dummy;
+  let second = dummy;
+
+  // Move first n+1 steps ahead
+  for (let i = 0; i <= n; i++) {
+    first = first.next;
+  }
+
+  // Move both until first reaches end
+  while (first) {
+    first = first.next;
+    second = second.next;
+  }
+
+  // Delete node
+  second.next = second.next.next;
+
+  return dummy.next;
+}
+
+
+// Complexity:
+// Time: O(n)
+// Space: O(1)
+
 
 
 
