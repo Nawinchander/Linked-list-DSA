ab83c55421148dd72f5a827a9beb2541ff5d2207
Nawin chander <nawinnapzy289@gmail.com>
2026-03-20 09:31:00 +0530

    add the code

--- Advance/Copy List with Random Pointer.js
+++ Advance/Copy List with Random Pointer.js
@@ -5,7 +5,57 @@
 // random pointer
 // You must clone the list without extra space (O(1))
 
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.random = null;
+  }
+}
 
+function copyRandomList(head) {
+  if (!head) return null;
+
+  let curr = head;
+
+  // Step 1: Clone nodes and insert in between
+  while (curr) {
+    let copy = new Node(curr.val);
+    copy.next = curr.next;
+    curr.next = copy;
+    curr = copy.next;
+  }
+
+  // Step 2: Assign random pointers
+  curr = head;
+  while (curr) {
+    if (curr.random) {
+      curr.next.random = curr.random.next;
+    }
+    curr = curr.next.next;
+  }
+
+  // Step 3: Separate lists
+  curr = head;
+  let newHead = head.next;
+
+  while (curr) {
+    let copy = curr.next;
+    curr.next = copy.next;
+    if (copy.next) {
+      copy.next = copy.next.next;
+    }
+    curr = curr.next;
+  }
+
+  return newHead;
+}
+
+
+// Complexity:
+
+// Time: O(n)
+// Space: O(1) (no hashmap used)
 
 
 
